f93d9103d03e8769c5f094f6542105511f4bce27
Allen Cunningham <cleverpiggy1@mac.com>
2014-04-29 12:58:10 -0700

    fixed package name in license headers

--- src/poker_lite.c
+++ src/poker_lite.c
@@ -1,18 +1,18 @@
 // Copyright 2013 Allen Boyd Cunningham
 
-// This file is part of pypoker-tools.
+// This file is part of pokyr.
 
-//     pypoker_tools is free software: you can redistribute it and/or modify
+//     pokyr is free software: you can redistribute it and/or modify
 //     it under the terms of the GNU General Public License as published by
 //     the Free Software Foundation, either version 3 of the License, or
 //     (at your option) any later version.
-//     pypoker_tools is distributed in the hope that it will be useful,
+//     pokyr is distributed in the hope that it will be useful,
 //     but WITHOUT ANY WARRANTY; without even the implied warranty of
 //     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 //     GNU General Public License for more details.
 
 //     You should have received a copy of the GNU General Public License
-//     along with pypoker_tools.  If not, see <http://www.gnu.org/licenses/>.
+//     along with pokyr.  If not, see <http://www.gnu.org/licenses/>.
 
 
 #include <stdint.h>
